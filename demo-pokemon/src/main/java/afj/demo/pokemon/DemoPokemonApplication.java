267fbfc60c9b545379cc8674a8a82b4436abbedb
alfredofonseca <alfredofonsecajunior@gmail.com>
2024-03-19 22:31:49 +0000

    Add files via upload
    initial commit

---
+++ demo-pokemon/src/main/java/afj/demo/pokemon/DemoPokemonApplication.java
@@ -0,0 +1,13 @@
+package afj.demo.pokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPokemonApplication.class, args);
+	}
+
+}
---
+++ demo-pokemon/src/test/java/afj/demo/pokemon/DemoPokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package afj.demo.pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
